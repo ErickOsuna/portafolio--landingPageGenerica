769cedc0d066772e65e45e0fed660cc33152c912
Erick Osuna Saavedra <erick_osuna@hotmail.com>
2023-05-07 17:25:38 -0300

    Segundo Commit

--- js/questions.js
+++ js/questions.js
@@ -1,6 +1,6 @@
-(function () {
-    
+(function(){
     const titleQuestions = [...document.querySelectorAll('.questions__title')];
+    console.log(titleQuestions)
 
     titleQuestions.forEach(question =>{
         question.addEventListener('click', ()=>{
@@ -9,15 +9,13 @@
             let addPadding = question.parentElement.parentElement;
 
             addPadding.classList.toggle('questions__padding--add');
-
             question.children[0].classList.toggle('questions__arrow--rotate');
 
             if(answer.clientHeight === 0){
                 height = answer.scrollHeight;
             }
-            answer.style.height = `${height}px`;
 
+            answer.style.height = `${height}px`;
         });
     });
-
 })();
